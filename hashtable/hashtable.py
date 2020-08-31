67f1171259a9e5edae6c12275a1c213ebb980f11
andrewbastian <andrew.bastian@gmail.com>
2020-08-31 14:56:10 -0700

    Hash Tables I - hash_index

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -82,15 +82,16 @@ class HashTable:
         Implement this, and/or FNV-1.
         """
         # Your code here
-
+        pass
 
     def hash_index(self, key):
         """
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        #return self.fnv1(key) % self.capacity
-        return self.djb2(key) % self.capacity
+        return self.fnv1(key) % self.capacity
+        # return self.djb2(key) % self.capacity
+
 
     def put(self, key, value):
         """
