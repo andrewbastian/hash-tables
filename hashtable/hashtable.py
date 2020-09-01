eb0b5a0a0cb520fc672389264b907d967727a0c6
andrewbastian <andrew.bastian@gmail.com>
2020-09-01 15:48:12 -0700

    Day 2 MVP - Chaining

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -7,6 +7,9 @@ class HashTableEntry:
         self.value = value
         self.next = None
 
+    def __repr__(self):
+        return f'NODE:({repr(self.value)})'
+
 
 # Hash table can't have fewer than this many slots
 MIN_CAPACITY = 8
@@ -20,11 +23,29 @@ class HashTable:
     Implement this.
     """
 
-    def __init__(self, capacity = MIN_CAPACITY):
+    def __init__(self, capacity=MIN_CAPACITY):
         # Your code here
         self.capacity = capacity
-        size = 0
+        self.head = 0
         self.buckets = [None] * self.capacity
+
+# DAY 2:
+    def __str__(self):
+        if self.head is None:
+            return '[EMPTY LIST]'
+
+        cur = self.head
+        string = ''
+
+        while cur is not None:
+            string += f'({cur.value})'
+
+            if cur.next is not None:
+                string += '-->'
+
+            cur = cur.next
+
+        return string
 
     def get_num_slots(self):
         """
@@ -37,8 +58,9 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
+# DAY 1
         return len(self.buckets)
+        
 
     def get_load_factor(self):
         """
@@ -47,9 +69,8 @@ class HashTable:
         Implement this.
         """
         # Your code here
-        loadf = self.size / self.capacity
+        return self.head / self.capacity
 
-        return loadf
 
     def fnv1(self, key):
         """
@@ -102,11 +123,49 @@ class HashTable:
         Implement this.
         """
         # Your code here
-        # Which slot (index) in the table is the value going?
-        idx = self.hash_index(key)
+        # DAY 1
+                # Which slot (index) in the table is the value going?
+        """        idx = self.hash_index(key)
         res = HashTableEntry(key, value)
         # Store the value at that slot
-        self.buckets[idx] = res
+        self.buckets[idx] = res"""
+        # DAY 2
+        
+        idx = self.hash_index(key)
+        res = HashTableEntry(key, value)
+        cur = self.buckets[idx]
+
+        # if the bucket has no index:
+        if self.buckets[idx] is None:
+            # add the new entry to the unoccupied index
+            self.buckets[idx] = HashTableEntry(key, value)
+            # expand LinkedList by 1 node
+            self.head += 1
+            # if the HashTable is getting too small,
+            if self.get_load_factor() > 0.7:
+                # expand the Table by a factor of `2` 
+                self.resize(self.capacity * 2)
+        else:
+            while cur:
+                # check the buckets index key against the entry key for equality,
+                if cur.key is key:
+                    # check the buckets index value against the entry value,
+                    cur.value = value
+                    # if both are true do not make an entry
+                    return
+                # create new var equal to the buckets index
+                prev = cur
+                # move that index to the next node
+                cur = cur.next
+            # set our entry to the node after the one we are currently on
+            prev.next = res
+            # expand LinkedList by 1 Node
+            self.head += 1
+            # if HashTable is getting too small
+            if self.get_load_factor() > 0.7:
+                # expand HashTable by a factor of `2`
+                self.resize(self.capacity * 2)
+
 
     def delete(self, key):
         """
@@ -146,6 +205,22 @@ class HashTable:
         """
         # Your code here
 
+        # DAY 2
+        new_table = HashTable(new_capacity)
+        # for each slot in table:
+        for head in self.buckets:
+            # for each element in the linked list in that slot:
+            if head:
+                # for each element in the linked list in that slot:
+                new_table.put(head.key, head.value)
+                if head.next:
+                    cur = head
+                    while cur.next:
+                        cur = cur.next
+                        new_table.put(cur.key, cur.value)
+
+        self.buckets = new_table.buckets
+        self.capacity = new_table.capacity
 
 
 if __name__ == "__main__":
