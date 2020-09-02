df5c1efc33827396622b1dae4b73556e77680e7e
andrewbastian <andrew.bastian@gmail.com>
2020-09-02 15:09:10 -0700

    Caesar crack

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -3,3 +3,31 @@
 
 # Your code here
 
+# import csv
+# with open('ciphertext.txt', 'r') as myCipther:
+#     lines = csv.DictReader(myCipther, delimiter=',')
+#     for line in lines:
+#         print(lines)
+
+from collections import defaultdict
+
+counts = defaultdict(int)
+
+
+with open('ciphertext.txt') as file:
+    ciphertext = file.read()
+
+for c in ciphertext:
+    if c.isupper():
+        counts[c] += 1
+
+freq_list = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U',
+             'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+
+cipher_freq = [item[0] for item in sorted(counts.items(),
+                                          key=lambda x: x[1],
+                                          reverse=True)]
+
+lookup = {cipher: plain for cipher, plain in zip(cipher_freq, freq_list)}
+
+print(ciphertext.translate(str.maketrans(lookup)))
